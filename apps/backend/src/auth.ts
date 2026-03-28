baa50f6ed36b36843bb941bb831a27eef07fb800
Bagaskaraaaa12 <h1101241023@student.untan.ac.id>
2026-03-29 06:33:37 +0700

    Mono4

--- apps/backend/src/auth.ts
+++ apps/backend/src/auth.ts
@@ -1,7 +1,6 @@
 import { google } from "googleapis";
 
 export function createOAuthClient() {
-
   return new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
